d31b28ee5d37b266414c36ffd62adb4b588d6faf
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-04 18:19:08 +0300

    ✔️ video 13: add bootstrap and jquery libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ECommerceClient';
+
+  ngOnInit(): void {
+    
+  }
 }

4bbe3c55ca8b576e38601a2d54fa3432704e6a6e
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-14 00:12:39 +0300

    ✔️ video 17: CORS basic get function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import {
   CustomToastrService,
   ToastrMessageType,
   ToastrPosition,
 } from './services/ui/custom-toastr.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { isPlatformBrowser } from '@angular/common';
 declare var $: any;
 
 @Component({
@@ -13,13 +14,22 @@ declare var $: any;
   standalone: false,
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ECommerceClient';
 
   constructor(
     private toastr: CustomToastrService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      $.get('https://localhost:7119/api/Products')
+        .done(response => console.log('Response:', response))
+        .fail((xhr, status, error) => {
+          console.error('Request failed:', { status, error, response: xhr.responseText });
+        });
+    }
+  }  
 }
